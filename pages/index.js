3c8822e3c5c7fb26ff50f6262e93de7c5c64dc29
DRashidov <rashydov.work@gmail.com>
2020-06-19 17:08:44 +0300

    add Gift sales section

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,26 @@
 import Head from 'next/head';
 import Layout from '../components/Layout';
 
+import styles from '../styles/brand.module.scss';
+
 export default function BrandPage() {
   return (
     <Layout>
       <Head>
         <title>Brand</title>
       </Head>
-      <h1>First Post</h1>
-      <h2>
-        Second div
-      </h2>
+      {/* Main Promo */}
+      <section></section>
+
+      {/* How it works */}
+      <section></section>
+
+      {/* Scale your gift sales */}
+      <section className={styles.giftSalesSection}>
+        <h1 className={styles.title}>Scale your gift sales.</h1>
+        <p className={styles.actionText}>Get started for free.</p>
+        <a className={styles.getStarted}>Get Started</a>
+      </section>
     </Layout>
   );
 }
