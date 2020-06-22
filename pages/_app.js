b86ff63d10aa745f11ee8e8dcd33884eda53c4f9
DRashidov <rashydov.work@gmail.com>
2020-06-22 13:02:55 +0300

    separate brand from home page

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,10 @@
-import '../styles/global.css';
+import Layout from '../components/Layout';
+import '../styles/global.scss';
 import Head from 'next/head';
 
 export default function App({ Component, pageProps }) {
   return (
-    <>
+    <Layout>
       <Head>
         <link
           href='https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&family=Noto+Sans+TC:wght@100;300;400;900&display=swap'
@@ -11,6 +12,6 @@ export default function App({ Component, pageProps }) {
         />
       </Head>
       <Component {...pageProps} />
-    </>
+    </Layout>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,57 +1,26 @@
 import Head from 'next/head';
-import Layout from '../components/Layout';
+import styles from '../styles/home.module.scss';
 
-import styles from '../styles/brand.module.scss';
-
-export default function BrandPage() {
+export default function Home() {
   return (
-    <Layout>
+    <>
       <Head>
-        <title>Brand</title>
+        <title>Home</title>
       </Head>
-      {/* Main Promo */}
-      <section className={styles.mainPromo}>
-        <div className={styles.text}>
-          <h1 className={styles.title}>Supercharge your gift <br/> guides.</h1>
-          <p className={styles.actionText}>Betabook helps brands sell gifts with influencers.</p>
-          <a className={styles.getStarted}>Get Started</a>
-        </div>
-        <div className={styles.image}>
-          <img src="/images/bg-card.png" alt="promo"/>
-        </div>
-      </section>
-
-      {/* How it works */}
-      <section className={styles.howItWorksSection}>
-        <h1 className={styles.title}>How it works</h1>
-        <div className={styles.cards}>
-          <div className={styles.card}>
-            <div className={styles.imgContainer}>
-              <img src="/images/gift.png" alt=""/>
-            </div>
-            <p className={styles.desc}>Build gift guides, earn money.</p>
-          </div>
-          <div className={styles.card}>
-            <div className={styles.imgContainer}>
-              <img src="/images/link.png" alt=""/>
-            </div>
-            <p className={styles.desc}>Share links to gifts you love.</p>
-          </div>
-          <div className={styles.card}>
-            <div className={styles.imgContainer}>
-              <img src="/images/dollar-sign.png" alt=""/>
-            </div>
-            <p className={styles.desc}>Earn 10% from brands on each sale.</p>
-          </div>
+      <section className={styles.homeMainPromo}>
+        <h1 className={styles.sectionTitle}>Find the perfect gift.</h1>
+        <p className={styles.sectionActionText}>
+          Shop gift guides with gurus, buy directly from brands.
+        </p>
+        <div className={styles.inputContainer}>
+          <input
+            type='text'
+            placeholder='Tell us who you’re gifting for'
+            className={styles.inputSearch}
+          />
+          <img src="/images/search-white.png" alt="search" className={styles.searchIcon} />
         </div>
       </section>
-
-      {/* Scale your gift sales */}
-      <section className={styles.giftSalesSection}>
-        <h1 className={styles.title}>Scale your gift sales.</h1>
-        <p className={styles.actionText}>Get started for free.</p>
-        <a className={styles.getStarted}>Get Started</a>
-      </section>
-    </Layout>
+    </>
   );
 }
