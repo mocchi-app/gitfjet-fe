700418249514728f7f9144385dfb62f6b1177928
DRashidov <rashydov.work@gmail.com>
2020-06-23 15:21:36 +0300

    add request for auth token

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,7 +1,28 @@
 import Link from 'next/link';
+import fetch from 'isomorphic-unfetch';
+
 import styles from './Header.module.scss';
 
 export default function Header() {
+
+  const handleSignUp = async () => {
+    const { API_URL } = process.env;
+    const proxyurl = "https://cors-anywhere.herokuapp.com/";
+
+    const res = await fetch(proxyurl + `${API_URL}/api/v1/brand/signup`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        companyUrl: "mocchi-app.myshopify.com"
+      })
+    });
+    const data = await res.json();
+
+    console.log('data', data);
+  }
+
   return (
     <header className={styles.topHeader}>
       <img src='/images/giftjet-logo.png' alt='logo' />
@@ -14,9 +35,9 @@ export default function Header() {
         <Link href='/sign-in'>
           <a className={styles.signInBtn}>Sign In</a>
         </Link>
-        <Link href='/sign-up'>
-          <a className={styles.signUpBtn}>Sign Up</a>
-        </Link>
+        {/* <Link href='/sign-up'> */}
+          <a className={styles.signUpBtn} onClick={handleSignUp}>Sign Up</a>
+        {/* </Link> */}
       </div>
     </header>
   );
