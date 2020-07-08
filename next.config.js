f670fd8d7971041334287af63a9a62a9a5fa1382
DRashidov <rashydov.work@gmail.com>
2020-07-08 15:45:52 +0300

    add sitemap generation

--- next.config.js
+++ next.config.js
@@ -5,7 +5,11 @@ module.exports = {
   env: {
     API_BRAND: "https://mocchi-brand-be.herokuapp.com",
   },
-  webpack: (config) => {
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      require("./scripts/generate-sitemap");
+    }
+    
     config.resolve.alias['components'] = path.join(__dirname, 'components');
     config.resolve.alias['styles'] = path.join(__dirname, 'styles');
     config.resolve.alias['public'] = path.join(__dirname, 'public');
